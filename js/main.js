664bb40b32fb92d3eee5ab227b905b460e1d4bf4
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2019-10-25 15:07:04 -0500

    finished with my styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-console.log('Hello');
+/*----- constants -----*/
+const player1 = o;
+const player2 = x;
+const wincombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+];
+    
+
+
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+
+
+/*----- functions -----*/
+
